1b1d08e471b6d55a140db9c82056738e62569c98
iwahid-resume <57856819+iwahid-resume@users.noreply.github.com>
2020-02-03 22:11:10 +0500

    Feat (all) add move functional
    Added a function to transfer an item from one list to another

--- resources/js/main.js
+++ resources/js/main.js
@@ -7,6 +7,13 @@ let complete_svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" 
 // Variables
 let new_task_input = document.getElementById("new-task__input") // Input value of using for create new task
 let add_new_item = document.getElementById("add-new-item") // create button
+// ul list-items (with them id)
+let todo_uncomplete = document.getElementById("todo__uncomplete")
+let todo_uncomplete_id = "todo__uncomplete"
+
+let todo_complete = document.getElementById("todo__complete")
+let todo_complete_id = "todo__complete"
+
 
 //add new item to todo list. First event listener (mouse click)
 add_new_item.addEventListener("click", function(){
@@ -34,11 +41,24 @@ function createNewItem (){
 
 // delete current item of the list-items (I doing in accordance with the principle: one function - one action. That comment add to docs of project (readmy))
 function deleteItem(){
-    let itemChild = this.parentNode.parentNode // current item. //
-    let parentOfItem = this.parentNode.parentNode.parentNode // parent of current child. // Depth of properties depends on the nesting of elements: li/buttons/button
+    let itemChild = this.parentNode.parentNode // current item
+    let parentOfItem = this.parentNode.parentNode.parentNode // parent of current child.// Depth of properties depends on the nesting of elements: li/buttons/button
     parentOfItem.removeChild(itemChild)
 }
 
+// transposition item from uncomplete into complete list. And back a round
+function transpositionItem (){
+    let itemChild = this.parentNode.parentNode // current item
+    let parentOfItem = this.parentNode.parentNode.parentNode //or item.parentNode // parent of current child 
+
+    //Determine from which list current element
+    let destenationList = (parentOfItem.id == todo_uncomplete_id) ? todo_complete : todo_uncomplete
+    
+    //transposition to "complete" list
+    
+    destenationList.insertBefore(itemChild, destenationList.childNodes[0])
+    console.log("item transposition from " + itemChild + " to " + destenationList)
+}
 
 
 // Function of create new item in ToDo-complete list Items
@@ -56,8 +76,9 @@ function addNewItemToList(textValue){
     let uncomplete_item_buttons = document.createElement("div")
     uncomplete_item_buttons.classList.add ("uncomplete-item__buttons") 
 
+    // buttons 
     let button_delete = document.createElement ('button')
-    button_delete.classList.add("uncomplete-item__button") // two classes
+    button_delete.classList.add("uncomplete-item__button")
     button_delete.classList.add("button-delete") // two classes
     button_delete.innerHTML = delete_svg // adding real HTML into html element
     
@@ -65,10 +86,13 @@ function addNewItemToList(textValue){
     button_delete.addEventListener("click", deleteItem)
 
     let button_complete = document.createElement("button")
-    button_complete.classList.add("uncomplete-item__button") //two classes
+    button_complete.classList.add("uncomplete-item__button") 
     button_complete.classList.add("button-complete") //two classes
     button_complete.innerHTML = complete_svg
-  
+
+    // event listener for "complete" button
+    button_complete.addEventListener("click", transpositionItem)
+    // buttons (end)
 
     // Adding now created items to parent items. Matryoshka-style princip. Do not forget about the sequence: first buttons of delete, second of complete
     uncomplete_item_buttons.appendChild(button_delete)
@@ -81,7 +105,7 @@ function addNewItemToList(textValue){
 
     uncomplete_list_item.insertBefore(uncomplete_item, uncomplete_list_item.childNodes[0]) // uncomplete_list_item.appendChild(uncomplete_item) //reverse sequence
     
-    console.log("Add new item and an includes that to uncomplete list")
+    console.log("Create new item and an includes that to uncomplete list")
 
     new_task_input.value="" // clear
 
